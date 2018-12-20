2ca38868ce196cfdd7cac44a1877389c956e9b01
Qshick <qshickkim@gmail.com>
2018-12-19 21:11:03 -0600

    Added docs files

--- docs/assets/scripts/App.js
+++
@@ -1,7 +0,0 @@
-import Project from './modules/Project';
-import Collapsible from './modules/Collapsible';
-import StickyHeader from './modules/StickyHeader';
-
-var project = new Project();
-var collapsible = new Collapsible();
-var stickyHeader = new StickyHeader();
